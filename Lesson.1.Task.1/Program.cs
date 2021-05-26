7165c2d32f76340aa90d2845b018e833dddc60d1
Vasilchikov <vasdm2018@yandex.ru>
2021-05-26 03:03:16 +0300

    Написать программу, выводящую в консоль текст: «Привет, %имя пользователя%, сегодня %дата%». Имя пользователя сохранить из консоли в промежуточную переменную. Поставить точку останова и посмотреть значение этой переменной в режиме отладки. Запустить исполняемый файл через системный терминал.

---
+++ Lesson.1.Task.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson._1.Task._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет " + name + ", сегодня [" + DateTime.Now + "]");// конкатенация
+            Console.WriteLine($"Привет {name}, сегодня [{DateTime.Now}] ");//интерполяция
+            Console.WriteLine("Привет {0}, сегодня [{1}]", name, DateTime.Now);
+
+            /*запуск из терминала:
+            вид > терминал > 
+            cd .. //переидти в каталог с проектом
+            dotnet run 
+             */
+        }
+    }
+}
