d555287bdb12e3018e422369260464718349d67c
Васильчиков Дмитрий <vasdm2017@mail.ru>
2022-06-08 17:26:11 +0300

    1. Написать программу, выводящую элементы двумерного массива по диагонали.

---
+++ Lesson.3.Task.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson._3.Task._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
